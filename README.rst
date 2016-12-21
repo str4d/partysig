deb8da1930b1975c4d6d3262933ac11aacc0ada5
str4d <str4d@mail.i2p>
2016-12-22 00:24:14 +1300

    Set up repository

---
+++ README.rst
@@ -0,0 +1,5 @@
+========
+partysig
+========
+
+A command-line tool for creating and verifying distributed multi-party signatures.
---
+++ setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) Jack Grigg
+# See LICENSE for details.
+
+from setuptools import setup
+
+
+with open('README.rst', 'rb') as infile:
+    long_description = infile.read()
+
+setup(
+    name='partysig',
+    version='0',
+    description='Create and verify distributed multi-party signatures',
+    long_description=long_description,
+    author='Jack Grigg',
+    author_email='str4d@i2pmail.org',
+    url='https://github.com/str4d/partysig',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Security',
+    ],
+    license='MIT',
+
+    install_requires=[
+        'magic-wormhole',
+    ],
+    packages=[
+        'partysig',
+    ],
+)
